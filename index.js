0cd6464916423c22bc83bac32c07e84d755d9af0
bsdaoquang <bsdaoquang@gmail.com>
2025-08-24 22:03:12 +0700

    run server ok

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 /** @format */
-console.log('Hello World!');
+
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
+
+app.listen(3000, (error) => {
+	if (error) {
+		console.error('Error starting server:', error);
+	} else {
+		console.log('Server is running on port http://localhost:3000');
+	}
+});

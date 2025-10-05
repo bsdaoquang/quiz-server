48f413605631c176dd08cde7ffe3d80edf1d2e1e
bsdaoquang <bsdaoquang@gmail.com>
2025-10-05 23:52:23 +0700

    curd questions

--- src/controllers/questions.js
+++ src/controllers/questions.js
@@ -1,7 +1,8 @@
 /** @format */
 
-import QuestionModel from '../models/QuestionModel.js';
+import QuestionModel, { CategoryModel } from '../models/QuestionModel.js';
 import asyncHandle from 'express-async-handler';
+import { replaceName } from '../utils/replaceName.js';
 
 // create a question
 const createQuestion = asyncHandle(async (req, res) => {
@@ -74,9 +75,26 @@ const getQuestions = asyncHandle(async (req, res) => {
 		QuestionModel.countDocuments(query),
 	]);
 
+	const promises = questions.map(async (question) => {
+		const categories = question.categories || [];
+		const populatedCategories = await Promise.all(
+			categories.map(async (categoryId) => {
+				// Assuming CategoryModel is imported and has a findById method
+				const category = await CategoryModel.findById(categoryId);
+				return category;
+			})
+		);
+		return {
+			...question.toObject(),
+			categories: populatedCategories,
+		};
+	});
+
+	const populatedQuestions = await Promise.all(promises);
+
 	return res.status(200).json({
 		message: 'get questions successfully',
-		data: { questions, total },
+		data: { questions: populatedQuestions, total },
 	});
 });
 
@@ -93,5 +111,34 @@ const deleteQuestions = asyncHandle(async (req, res) => {
 		.status(200)
 		.json({ message: 'Questions deleted successfully', data: ids });
 });
+const categories = ['68dea190089bfdfc366927a1'];
+const demoQuestions = [];
 
-export { createQuestion, createManyQuestions, getQuestions, deleteQuestions };
+const addDemoQuestions = asyncHandle(async (req, res) => {
+	const fixedQuestions = demoQuestions.map((q) => {
+		return {
+			...q,
+			slug: replaceName(q.question),
+			correctAnswer: q.answer,
+			createdBy: '68b1c699d4346052479d3a80',
+		};
+	});
+
+	try {
+		await QuestionModel.insertMany(fixedQuestions);
+		return res.status(200).json({
+			message: 'Demo questions added successfully',
+			data: fixedQuestions,
+		});
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+export {
+	createQuestion,
+	createManyQuestions,
+	getQuestions,
+	deleteQuestions,
+	addDemoQuestions,
+};
